8f9ed821aee041637260a53d1a31eb615cd7c316
Abu Sayem Amir <sayem74@gmail.com>
2022-04-15 22:15:27 +0600

    initial commit

---
+++ assignment6.php
@@ -0,0 +1,29 @@
+<?php 
+    /* 
+        Question 01:
+        You have purchased some items from a supershop. Purchase product array is given below-
+
+        $purchased_products = array(
+        		array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
+        		array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
+        		array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
+    	);
+            How much product quantity you have purchased (total purchased qty = 67)
+            Calculate the total amount of all purchased items (total amount = 2650 tk).
+    */
+
+        // $purchased_products = array(
+        // 		array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
+        // 		array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
+        // 		array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
+    	// );
+
+        // $sum = array_sum($purchased_products);
+
+        // echo $sum;
+
+        $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+echo  array_sum($b) . "\n";
+
+
+?>
