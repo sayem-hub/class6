31b50ea224e9bbc2f0f014f71272d2f57349e159
Abu Sayem Amir <sayem74@gmail.com>
2022-04-16 21:08:44 +0600

    completed question no.1

--- assignment6.php
+++ assignment6.php
@@ -12,18 +12,28 @@
             Calculate the total amount of all purchased items (total amount = 2650 tk).
     */
 
-        // $purchased_products = array(
-        // 		array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
-        // 		array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
-        // 		array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
-    	// );
+            $purchased_products = array(
+                array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
+                array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
+                array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
+                );
 
-        // $sum = array_sum($purchased_products);
+                    $total_qty  =  0;
+                    $total_amount  = 0;
 
-        // echo $sum;
+                foreach($purchased_products as $key => $qty) {
+   
+                        $total_qty += $qty ['qty'];
+                    }
+                        echo "Total purchased qty = ". $total_qty . " Pcs". "<br>";
 
-        $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
-echo  array_sum($b) . "\n";
+                        foreach ($purchased_products as $key => $price) {
+                            $total_amount += $price ['total_price'];
+                        }
 
+                        echo "Total amount = " .$total_amount. " Tk.";
+                        /*Output 
+                        Total purchased qty = 67 Pcs
+                        Total amount = 2650 Tk.*/
 
 ?>
